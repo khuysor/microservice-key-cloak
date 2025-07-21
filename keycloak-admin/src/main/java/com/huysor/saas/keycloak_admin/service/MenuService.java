e57d83dae0587c0888eb7cacd4c52ae15d39aadc
khuysor <huysorkheang22@gmail.com>
2025-07-21 23:06:14 +0700

    refactor name

--- keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/service/MenuService.java
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/service/MenuService.java
@@ -3,19 +3,19 @@ package com.huysor.saas.keycloak_admin.service;
 import com.huysor.saas.common.dto.res.ApiRes;
 import com.huysor.saas.keycloak_admin.dto.req.MenuReq;
 import com.huysor.saas.keycloak_admin.dto.resp.MenuDetailResp;
-import com.huysor.saas.keycloak_admin.dto.resp.MenuResp;
+import com.huysor.saas.keycloak_admin.dto.resp.MenuRes;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface MenuService {
-    ResponseEntity<ApiRes<MenuResp>> createMenu(MenuReq menuReq);
+    ResponseEntity<ApiRes<MenuRes>> createMenu(MenuReq menuReq);
 
-    ResponseEntity<ApiRes<List<MenuResp>>> listAllMenu();
+    ResponseEntity<ApiRes<List<MenuRes>>> listAllMenu();
 
-    ResponseEntity<ApiRes<MenuResp>> updateMenu(MenuReq menuReq);
+    ResponseEntity<ApiRes<MenuRes>> updateMenu(MenuReq menuReq);
 
     ResponseEntity<ApiRes<MenuDetailResp>> findMenuById(Long id);
 
-    List<MenuResp> findMenuByUserId(Long id, Integer languageId);
+    List<MenuRes> findMenuByUserId(Long id, Integer languageId);
 }
