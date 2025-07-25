17167ed789d60db0c43d5795c0c23d2a4df6aa88
khuysor <huysorkheang22@gmail.com>
2025-07-25 16:34:07 +0700

    throw exception when user input wrong request body

--- keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/exception/GlobalException.java
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/exception/GlobalException.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.HttpResponseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,13 +20,13 @@ public class GlobalException {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiRes<String>> handleNotFound(NotFoundException ex) {
         log.error("Resource not found: {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiRes.error("Resource not found",null,HttpStatus.NOT_FOUND.value()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiRes.error("Resource not found", null, HttpStatus.NOT_FOUND.value()));
     }
 
     @ExceptionHandler()
     public ResponseEntity<ApiRes<String>> handleNotFound(NoResourceFoundException ex) {
         log.error("not found: {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiRes.error("Resource not found",null,HttpStatus.NOT_FOUND.value()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiRes.error("Resource not found", null, HttpStatus.NOT_FOUND.value()));
     }
 
     @ExceptionHandler(BadRequestException.class)
@@ -44,13 +45,19 @@ public class GlobalException {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiRes<String>> handleGenericException(Exception ex) {
         log.error("An unexpected error occurred: {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiRes.error("An unexpected error occurred",null,HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiRes.error("An unexpected error occurred", null, HttpStatus.INTERNAL_SERVER_ERROR.value()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiRes<String>> handleUnreadableBody(HttpMessageNotReadableException ex) {
+        log.error("Can't read json body {}", ex.getLocalizedMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiRes.error("Invalid Request Input", null, HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
 
     @ExceptionHandler(AuthorizationDeniedException.class)
     public ResponseEntity<ApiRes<String>> handleUnAuthorization(AuthorizationDeniedException ex) {
         log.error("don't have permission to access this resource: {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiRes.error("Your don't have permission to access this resource !!!",null,HttpStatus.FORBIDDEN.value()));
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiRes.error("Your don't have permission to access this resource !!!", null, HttpStatus.FORBIDDEN.value()));
 
     }
 
@@ -58,6 +65,6 @@ public class GlobalException {
     public ResponseEntity<ApiRes<String>> handleForbidden(ForbiddenException ex) {
         log.error("Access forbidden: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(ApiRes.error("You don't have permission to access this resource !!!",null,HttpStatus.FORBIDDEN.value()));
+                .body(ApiRes.error("You don't have permission to access this resource !!!", null, HttpStatus.FORBIDDEN.value()));
     }
 }
