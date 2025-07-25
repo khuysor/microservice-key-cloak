b731e0f52eb5361d5d5724c0f231604224451c53
khuysor <huysorkheang22@gmail.com>
2025-07-25 16:37:57 +0700

    create role filter

---
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/req/user/RoleFilter.java
@@ -0,0 +1,15 @@
+package com.huysor.saas.keycloak_admin.dto.req.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.huysor.saas.common.utils.PaginationFilter;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class RoleFilter extends PaginationFilter {
+    @JsonProperty(value = "name")
+    private String name;
+
+}
