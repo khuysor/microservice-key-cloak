86c4ad282f1fbce99b5d55d923a7a838190b784d
khuysor <huysorkheang22@gmail.com>
2025-07-25 16:38:20 +0700

    not response client id

--- keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/resp/PermissionRes.java
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/resp/PermissionRes.java
@@ -1,6 +1,6 @@
 package com.huysor.saas.keycloak_admin.dto.resp;
 
 
-public record PermissionRes(Long id, String name, String description, String keyCloakId) {
+public record PermissionRes(Long id, String name, String description) {
 
 }
