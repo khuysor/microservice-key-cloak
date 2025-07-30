58ba54b8dca7d51e6ff6cf94806bbf1b1f2c9ee7
khuysor <huysorkheang22@gmail.com>
2025-07-31 01:11:06 +0700

    using loki loging

--- api-product/src/main/java/com/huysor/saas/api/product/controller/ProductController.java
+++ api-product/src/main/java/com/huysor/saas/api/product/controller/ProductController.java
@@ -4,16 +4,18 @@ import com.huysor.saas.api.product.dto.req.ProductReq;
 import com.huysor.saas.api.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+
 @RestController
 @RequestMapping("products")
 @RequiredArgsConstructor
 public class ProductController {
     public final ProductService productService;
-
+    private final Logger log = LoggerFactory.getLogger(ProductController.class);
     @PostMapping("/create")
     public ResponseEntity<?> createProduct(@RequestBody ProductReq req) {
         return productService.createProduct(req);
@@ -30,5 +32,10 @@ public class ProductController {
         }
 
         return ResponseEntity.ok("CPU load generated for " + duration + "ms");
+    }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable("id") Long id) {
+        log.info("Fetching product with ID: {}", id);
+        return ResponseEntity.ok("Success");
     }
 }
