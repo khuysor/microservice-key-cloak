291c11261e7637a6955e7e23d772b06e938ca3c2
khuysor <huysorkheang22@gmail.com>
2025-08-01 15:01:53 +0700

    role response

--- keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/resp/RoleRes.java
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/resp/RoleRes.java
@@ -1,10 +1,20 @@
 package com.huysor.saas.keycloak_admin.dto.resp;
 
+import com.huysor.saas.keycloak_admin.entity.Role;
+
 import java.util.List;
+
 
 public record RoleRes(Long id,
                       String name,
                       String description,
                       List<PermissionRes> permissions) {
-
+    static RoleRes toRes(Role role) {
+        return new RoleRes(
+                role.getId(),
+                role.getName(),
+                role.getDescription(),
+                role.getPermissions().stream().map(PermissionRes::toRes).toList()
+        );
+    }
 }
