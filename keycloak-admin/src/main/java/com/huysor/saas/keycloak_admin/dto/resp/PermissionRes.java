c2006acf4dace01f823eb55a1b87bf45c1776f4a
khuysor <huysorkheang22@gmail.com>
2025-08-01 15:01:03 +0700

    permission res

--- keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/resp/PermissionRes.java
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/resp/PermissionRes.java
@@ -1,6 +1,14 @@
 package com.huysor.saas.keycloak_admin.dto.resp;
 
 
-public record PermissionRes(Long id, String name, String description) {
+import com.huysor.saas.keycloak_admin.entity.Permissions;
 
+public record PermissionRes(Long id, String name, String description) {
+    static PermissionRes toRes(Permissions permissions){
+        return new PermissionRes(
+                permissions.getId(),
+                permissions.getName(),
+                permissions.getDescription()
+        );
+    }
 }
