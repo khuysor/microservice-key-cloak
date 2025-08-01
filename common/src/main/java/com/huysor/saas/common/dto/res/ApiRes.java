0412ea87e23ce8a7968c3faa931523d337f23653
khuysor <huysorkheang22@gmail.com>
2025-08-01 15:00:52 +0700

    create not found response

--- common/src/main/java/com/huysor/saas/common/dto/res/ApiRes.java
+++ common/src/main/java/com/huysor/saas/common/dto/res/ApiRes.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Data
 
@@ -17,33 +19,45 @@ public class ApiRes<T> {
     private T data;
 
 
-    public static  <T> ApiRes<T> success(String message, T data) {
+    public static <T> ApiRes<T> success(String message, T data) {
         return of(message, 200, data);
     }
 
     public static <T> ApiRes<T> success(String message, T data, Integer statusCode) {
         return of(message, statusCode, data);
     }
-    public  static <T> ApiRes<T> success(T data) {
+
+    public static <T> ApiRes<T> success(T data) {
         return of("Success", 200, data);
     }
+
     public static <T> ApiRes<T> created() {
         return of("Successfully created", 201, null);
     }
 
-    public static  <T> ApiRes<T> error(String message, T data) {
+    public static <T> ApiRes<T> error(String message, T data) {
         return of(message, 500, data);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public static <T> ApiRes<T> notFound(String message) {
+        return of(message, 404, null);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public static <T> ApiRes<T> notFound() {
+        return of("Not found", 404, null);
     }
 
     public static <T> ApiRes<T> error(String message, T data, Integer statusCode) {
         return of(message, statusCode, data);
     }
 
-    public static  <T> ApiRes<T> error(String message) {
+    public static <T> ApiRes<T> error(String message) {
         return of(message, 500, null);
     }
 
-    public static  <T> ApiRes<T> error(String message, Integer statusCode) {
+    public static <T> ApiRes<T> error(String message, Integer statusCode) {
         return of(message, statusCode, null);
 
     }
