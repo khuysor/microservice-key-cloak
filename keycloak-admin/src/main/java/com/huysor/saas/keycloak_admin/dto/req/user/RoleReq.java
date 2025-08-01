4ad3ae012fc02673e382cd25e6f73c4019c4c28b
khuysor <huysorkheang22@gmail.com>
2025-08-01 15:01:39 +0700

    change field request from api

--- keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/req/user/RoleReq.java
+++ keycloak-admin/src/main/java/com/huysor/saas/keycloak_admin/dto/req/user/RoleReq.java
@@ -1,6 +1,6 @@
 package com.huysor.saas.keycloak_admin.dto.req.user;
 
-import java.util.List;
+import java.util.Set;
 
-public record RoleReq(Long id, String name, String roleDescription, List<Long> permissionsId) {
+public record RoleReq(Long id, String name, String roleDescription, Set<Long> permissionsId) {
 }
