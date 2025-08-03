fd03b6438a28e284068897de45ca0635a8e65816
khuysor <huysorkheang22@gmail.com>
2025-08-03 23:02:01 +0700

    testing logging and gateway

--- api-product/src/main/java/com/huysor/saas/api/product/controller/ProductController.java
+++ api-product/src/main/java/com/huysor/saas/api/product/controller/ProductController.java
@@ -3,11 +3,15 @@ package com.huysor.saas.api.product.controller;
 import com.huysor.saas.api.product.dto.req.ProductReq;
 import com.huysor.saas.api.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 @RestController
@@ -34,8 +38,23 @@ public class ProductController {
         return ResponseEntity.ok("CPU load generated for " + duration + "ms");
     }
     @GetMapping("/get/{id}")
-    public ResponseEntity<?> getProduct(@PathVariable("id") Long id) {
-        log.info("Fetching product with ID: {}", id);
-        return ResponseEntity.ok("Success");
+    public ResponseEntity<?> getProduct(@PathVariable("id") Long id ,@RequestHeader("X-Correlation-ID") String correlationId) throws InterruptedException {
+        int threadCount = 10;
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> {
+                long startTime = System.currentTimeMillis();
+                long endTime = startTime + id;
+                while (System.currentTimeMillis() < endTime) {
+                    Math.sqrt(Math.random() * 1000000);
+                }
+                latch.countDown();
+            });
+        }
+        latch.await();
+        executor.shutdown();
+        return ResponseEntity.ok("Success : "+ id + " Correlation ID: " + correlationId);
     }
 }
