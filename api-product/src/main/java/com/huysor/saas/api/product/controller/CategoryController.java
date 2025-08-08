286c9d653a8d88c2bddb5cac29a72037eb53f8c7
khuysor <huysorkheang22@gmail.com>
2025-08-09 00:36:50 +0700

    test request by fiegn client

--- api-product/src/main/java/com/huysor/saas/api/product/controller/CategoryController.java
+++ api-product/src/main/java/com/huysor/saas/api/product/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.huysor.saas.api.product.controller;
 
 import com.huysor.saas.api.product.httpClient.UserClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +14,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("")
+@RequiredArgsConstructor
+@Slf4j
 public class CategoryController {
     private final UserClient userClient;
 
-    public CategoryController(UserClient userClient) {
-        this.userClient = userClient;
-    }
+
     @GetMapping("/category")
-    public ResponseEntity<?> testFeignClient(){
+    public ResponseEntity<?> testFeignClient(@RequestHeader(value = "X-Correlation-ID") String correlationId ) {
         Map<String,Object> filter = new HashMap<>();
         filter.put("name","");
+        log.info("Correlation ID: {}", correlationId);
         var user = userClient.getAllUsers(filter);
         return ResponseEntity.ok(user);
     }
